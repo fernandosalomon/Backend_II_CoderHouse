9f0da65cffa780400378dd1de0c237e98b687678
Fernando F. Salomon <fernandosalomon1992@gmail.com>
2026-04-21 15:02:59 -0300

    feat: add login and register views

--- src/app.js
+++ src/app.js
@@ -13,8 +13,12 @@ app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "./views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(SERVER_PORT, () => {
