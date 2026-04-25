365f5944adb01a66214fe8e3e07cf25e3149cd85
fernandosalomon <fernandosalomon1992@gmail.com>
2026-04-25 13:42:28 -0300

    feat: add required validation for email, password, and provider fields in User model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
   email: {
     type: String,
     unique: true,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
   role: {
     type: String,
@@ -16,6 +24,7 @@ const userSchema = mongoose.Schema({
   provider: {
     type: String,
     enum: ["local", "github", "google"],
+    required: true,
   },
 });
 
